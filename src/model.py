084c39316fb66763eae48e1a69ef17d80062b6e1
Logan <logan.lieou@protonmail.com>
2021-10-15 00:44:30 -0500

    something happened?

--- src/model.py
+++ src/model.py
@@ -4,10 +4,15 @@ import torch.nn.functional as F
 
 # neural network (do research into this for OCR)
 class Network(nn.Module):
-    def __init__(self):
+    def __init__(self, in_channels: int = 4):
         super(Network, self).__init__()
-        self.conv1 = nn.Conv2d()
-        self.relu  = nn.ReLU()
+        self.p = lambda x: (x[0] // 2, x[1] //2)
+        self.main = nn.Sequential(
+            nn.Conv2d(in_channels, 16, kernel_size=(3, 3), padding=self.p((3, 3))),
+            nn.ReLU(),
+            nn.MaxPool2d((2, 2), padding=self.p((2, 2))),
+            nn.Conv2d(16, 32, kernel_size=(3, 3), padding=self.p((3, 3))),
+        )
 
     def forward(self):
         pass
